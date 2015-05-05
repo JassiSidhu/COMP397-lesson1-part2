0ce49f5825aeae7dc25cc0ea2ac4bac73c0f6b3c
JassiSidhu <jassi.kaur667@gmail.com>
2015-05-05 10:52:24 -0400

    initial commit

---
+++ Comp397-lesson1-part2/scripts/game.js
@@ -0,0 +1,16 @@
+﻿var Person = (function () {
+    //constructor
+    function Person() {
+    }
+    //public methods
+    Person.prototype.saysHello = function () {
+        console.log("Hello!");
+    };
+    return Person;
+})();
+
+function init() {
+    var myPerson = new Person();
+    myPerson.saysHello();
+}
+//# sourceMappingURL=game.js.map
---
+++ Comp397-lesson1-part2/scripts/game.ts
@@ -0,0 +1,17 @@
+﻿class Person {
+    //constructor
+    constructor() {
+    }
+    //public methods
+    public saysHello() {
+        console.log("Hello!");
+    }
+}
+
+function init()
+{
+    var myPerson = new Person();
+    myPerson.saysHello();
+}
+
+
